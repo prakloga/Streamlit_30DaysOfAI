63b194cec04c21b5f5cdcd843f052914ae964df1
Prakash Loganathan <prakash.loganathaan@gmail.com>
2026-01-05 18:57:59 -0600

    Day4 Scripts

---
+++ streamlit_app_day2.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from snowflake.snowpark.functions import ai_complete
+import json
+
+st.title(":material/smart_toy: Hello Cortex!")
+
+# Connect to Snowflake
+try:
+    # works in Streamlit in Snowflake
+    from snowflake.snowpark.context import get_active_session
+    session = get_active_session()
+
+except:
+    # Works locally and Streamlit Community Cloud
+    from snowflake.snowpark import Session
+    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
+
+# Model and Prompts
+model = "claude-3-5-sonnet"
+prompt = st.text_input("Enter your prompt:")
+
+# Run LLM Inference
+if st.button("Generate Response"):
+    df = session.range(1).select(
+        ai_complete(model=model
+                    , prompt=prompt
+                    , model_parameters={"temperature": 0.7}
+                    ).alias("response")
+    )
+
+    # Get and display response
+    response_raw = df.collect()[0][0]
+    response = json.loads(response_raw)
+    st.write(response)
+
+# Footer
+st.divider()
+st.caption("Day 2: Hello, Cortex! | 30 days of AI")
